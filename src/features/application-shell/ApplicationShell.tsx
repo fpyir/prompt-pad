cc4f1bba2d9985c6a93e0c9babf5c012bb0f405d
fpyir <adam010101@icloud.com>
2023-06-10 16:03:59 +1000

    use ChatGPT to create a ProfileMenu component

--- src/features/application-shell/ApplicationShell.tsx
+++ src/features/application-shell/ApplicationShell.tsx
@@ -1,14 +1,10 @@
 "use client";
-import React, { Fragment, useState } from "react";
-import classNames from "classnames";
-import { Dialog, Menu, Transition } from "@headlessui/react";
+import React, { useState } from "react";
 import { SidebarLayout } from "./SidebarLayout";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import {
-  ChevronDownIcon,
-  MagnifyingGlassIcon,
-} from "@heroicons/react/24/solid";
+import { Bars3Icon, BellIcon } from "@heroicons/react/24/outline";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { PanelOverlay } from "../transitions/PanelOverlay";
+import ProfileMenu from "./ProfileMenu";
 
 export type ApplicationShellProps = {
   children: React.ReactNode;
@@ -85,55 +81,10 @@ export const ApplicationShell: React.FC<ApplicationShellProps> = ({
               />
 
               {/* Profile dropdown */}
-              <Menu as="div" className="relative">
-                <Menu.Button className="-m-1.5 flex items-center p-1.5">
-                  <span className="sr-only">Open user menu</span>
-                  <img
-                    className="h-8 w-8 rounded-full bg-gray-50"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                  <span className="hidden lg:flex lg:items-center">
-                    <span
-                      className="ml-4 text-sm font-semibold leading-6 text-gray-900"
-                      aria-hidden="true"
-                    >
-                      Tom Cook
-                    </span>
-                    <ChevronDownIcon
-                      className="ml-2 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </Menu.Button>
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                    {userNavigation.map((item) => (
-                      <Menu.Item key={item.name}>
-                        {({ active }) => (
-                          <a
-                            href={item.href}
-                            className={classNames(
-                              active ? "bg-gray-50" : "",
-                              "block px-3 py-1 text-sm leading-6 text-gray-900"
-                            )}
-                          >
-                            {item.name}
-                          </a>
-                        )}
-                      </Menu.Item>
-                    ))}
-                  </Menu.Items>
-                </Transition>
-              </Menu>
+              <ProfileMenu
+                name="Tom Cook"
+                profileImgSrc="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              />
             </div>
           </div>
         </div>
